32998dcf861e827371acd8ed7cb198a61ba396ff
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-28 13:42:20 -0500

    added files

---
+++ js/global.js
@@ -0,0 +1,23 @@
+//Once you've created the layout, write JavaScript to make 12 search results appear. You'll write a single JavaScript function that takes a single object as its argument. Within that function, you'll use that object to dynamically build the HTML needed to produce one search result card. And once you build that, within the function you'll append it to your search results container.
+
+createCard({
+//     src: 'http://unsplash.it/400/300?image=500',
+//     caption: 'Unknown Image'
+// });
+// createCard({
+//     src: 'http://unsplash.it/400/300?image=600',
+//     caption: 'Mystery'
+// });
+// createCard({
+//     src: 'http://unsplash.it/400/300?image=700',
+//     caption: 'Something'
+// });
+// function createCard(image) {
+//     var card = `<div class= "card">
+//                 <img src="${image.src}" alt= "Image"/>
+//                 <h5 class= "card-capton">${image.caption}</h5>
+//         </div>`;
+//     document.querySelector('#app').innerHTML += card;    
+// }
+
+
